dc845bc14a0e3d521daed25da099ed12d3d95e49
Emad Bin Abid <emad.bin.abid@gmail.com>
2021-11-27 23:23:03 +0500

    Add controller and service logic to find flights with minimum travel time.

--- apps/flight/controller/flight.controller.js
+++ apps/flight/controller/flight.controller.js
@@ -2,11 +2,26 @@ var express = require('express');
 
 var routes = require('../route/flight.route').flightRoutes
 var app = require('../server/flight.server').getAppInstance()
+var flightService = require('../service/flight.service')
 
 //Middlewares
 app.use(express.json());
 
 
 app.get(routes.getLowestTravelTimeFlights, (req, res) => {
-    res.json([])
+    // TODO: validate the request payload and make sure all fields are present
+
+    flightService.getFlightsWithLowestTravelTime(req.body.from, req.body.to, req.body.date)
+    .then((result) => {
+        // no record found
+        if (result == null || result == undefined) {
+            res.json({})
+        }
+        res.json(result)
+    })
+    .catch((err) => {
+        res.json({
+            "error": err
+        })
+    })
 });
---
+++ apps/flight/service/flight.service.js
@@ -0,0 +1,46 @@
+var arangojs = require('arangojs');
+var aql = arangojs.aql
+
+var dbManager = require('../../db/arangodb/manager')
+
+exports.getFlightsWithLowestTravelTime = function(fromAirport, toAirport, dateOfTravel) {
+    const db = dbManager.getArangoDBInstance()
+
+    const from = `airports/${fromAirport}`
+    const to = `airports/${toAirport}`
+
+    const date = dateOfTravel.split('/')
+    const year = parseInt(date[0])
+    const month = parseInt(date[1])
+    const day = parseInt(date[2])
+
+    const query = `
+                    FOR v, e, p IN 2 OUTBOUND @from flights
+                    FILTER v._id == @to
+                    FILTER p.edges[*].Year ALL == @year
+                    FILTER p.edges[*].Month ALL == @month
+                    FILTER p.edges[*].Day ALL == @day
+                    LET flightTime = DATE_DIFF(p.edges[0].DepTimeUTC, p.edges[1].ArrTimeUTC, 'i')
+                    SORT flightTime ASC
+                    LIMIT 5
+                    RETURN { flight: p, time: flightTime }
+                    `
+    const bindVars = {
+        from: from,
+        to: to,
+        year: year,
+        month: month,
+        day: day
+    }
+    
+
+    return db.query(query, bindVars)
+    .then((cursor) => {
+        return cursor.next().then(function(result) {
+          return result
+        });
+      })
+    .catch((err) => {
+        return err
+    })
+}
