0d047e5aa13acfbe733490b41da3a0f8b9fcd347
Emad Bin Abid <emad.bin.abid@gmail.com>
2021-11-28 13:24:26 +0500

    Make database name configurable as well.
    The database name should be configurable because it is a valid usecase that someone wants to use our application with their own data/database/data source. So, they can just configure the database and use our application base only.

--- apps/db/arangodb/config.js
+++ apps/db/arangodb/config.js
@@ -1,7 +1,7 @@
 const config = {
-    database: 'fly-time',
-    user: 'root',       // default user, if not set in env
-    password: 'root'    // default password, if not set in env
+    database: 'fly-time',   // default database, if not set in env
+    user: 'root',           // default user, if not set in env
+    password: 'root'        // default password, if not set in env
 }
 
 exports.db = config
--- apps/db/arangodb/manager.js
+++ apps/db/arangodb/manager.js
@@ -6,9 +6,10 @@ exports.getArangoDBInstance = function() {
 
     const user = process.env.ARANGO_DB_USER ? process.env.ARANGO_DB_USER : arangoDbConfig.db.user
     const password = process.env.ARANGO_DB_PASSWORD ? process.env.ARANGO_DB_PASSWORD : arangoDbConfig.db.password
+    const database = process.env.ARANGO_DB_NAME ? process.env.ARANGO_DB_NAME : arangoDbConfig.db.database
 
     db.useBasicAuth(user, password)
-    db.useDatabase(arangoDbConfig.db.database)
+    db.useDatabase(database)
 
     return db
 }
